fca720a9700a56b706e0f4598809dddb4e505ab8
jaredsbt <jaredsbt@gmail.com>
2023-01-17 21:45:34 -0800

    PA1 completed

---
+++ CompileTimeErrors.java
@@ -0,0 +1,17 @@
+import tester.*;
+
+class CompileTimeErrors{
+    
+    // calculate the area of a right triangle
+    double rightTriangleArea(double base, double height){
+        return base * height / 2;
+    }
+    
+    // square a number
+    int squareNumber(int myNumber){
+        return myNumber * myNumber;
+    }
+  
+    double answer1 = this.rightTriangleArea(3, 5); // expected 7.5
+    int answer2 = this.squareNumber(5); // expected 25
+}
